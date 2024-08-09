9200747aa6691dfce6d88bd027d5f2f821093759
Sean <92123184+seanmcosker@users.noreply.github.com>
2024-08-09 21:46:49 +0000

    adding more pages

--- pages/Sean Chat.py
+++ pages/Sean Chat.py
@@ -3,3 +3,10 @@ st.title("Sean Chat :wave:")
 st.text("If you're feeling lazy, use the chat below to ask questions about me and my experience.")
 st.text("This is powered by a RAG workflow... if you're curious about this, all code is in my github repo")
 st.chat_input("I'm your personal Sean expert!")
+
+
+OPENAI_API_KEY = ""
+
+if not OPENAI_API_KEY:
+    with st.sidebar:
+        OPENAI_API_KEY = st.text_input(label = "Enter API key")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
 st.title("Sean McOsker")
-st.write('Personal Website for Sean McOsker'
-    )
+#st.subheader('Personal Website for Sean McOsker')
+
+
+st.header("About Me")
+st.markdown("I'm a young professional with a background in statistics, which I've applied to the AI/ML space. Most recently I've been working as an AI Engineer in the energy storage space, with particular focus on better RAG workflows for LLM-powered chatbots. See some of my work below!")
+
+st.subheader("2024")
+st.markdown("AI Engineer, Fluence")
+st.markdown("Built and deployed My Domain Expert, a AI-powered chatbot trained to answer questions on internal company documentation and public market documents.")
 
